fefef65e85bfa130512ed197e8367ccff8bcd5ee
Bishtu <107715043+BishtuDas@users.noreply.github.com>
2024-01-03 16:29:36 +0530

    Add files via upload

---
+++ core/core/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('full_name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        data = {
+                'name': name,
+                'email':email,
+                'subject':subject,
+                'message': message
+        }
+        message = '''
+        New message: {}
+        From: {}
+        '''.format(data['message'],data['email'])
+        send_mail(data['subject'],message,'',['bistudas.das@gmail.com'])
+        
+    return render(request, 'core/index.html')
